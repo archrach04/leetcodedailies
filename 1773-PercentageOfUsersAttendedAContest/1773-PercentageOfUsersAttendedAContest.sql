382a1efe36da21d5dceac794af4051ac86407aed
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-03-28 00:54:13 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,8 @@
+-- Last updated: 3/28/2026, 12:54:10 AM
+SELECT 
+    R.contest_id,
+    ROUND(COUNT(R.user_id) * 100.0 / 
+          (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register R
+GROUP BY R.contest_id
+ORDER BY percentage DESC, R.contest_id ASC;
