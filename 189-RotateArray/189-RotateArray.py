0ae75c49acaae2326ad216f8f6f9a2296a730cfb
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-03-28 00:55:08 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.py
@@ -0,0 +1,17 @@
+# Last updated: 3/28/2026, 12:55:05 AM
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k=k%len(nums)
+        l,r=0,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        
+        l,r=0,k-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        l,r=k,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
