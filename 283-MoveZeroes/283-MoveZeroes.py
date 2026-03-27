f59364d9853111994d2441be1bfbfa9a10848890
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-03-28 00:54:50 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,11 @@
+# Last updated: 3/28/2026, 12:54:47 AM
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
