98536bd962192797c28f9ab0f7e6104ebefbac62
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-03-28 00:55:27 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,20 @@
+# Last updated: 3/28/2026, 12:55:24 AM
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        x=m-1
+        y=n-1
+
+        for z in range(m+n-1,-1,-1):
+            if x<0:
+                nums1[z]=nums2[y]
+                y-=1
+            elif y<0:
+                break      
+            elif nums1[x]>nums2[y]:
+                nums1[z]=nums1[x]
+                x-=1
+            else:
+                nums1[z]=nums2[y]
+                y-=1
+
+
