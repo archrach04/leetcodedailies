b26bda333bca288ce70e12f8137d4c6fc2678eec
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-03-28 21:35:08 +0530

    File updated at 3/28/2026, 9:35:05 PM

--- 621-TaskScheduler/621-TaskScheduler.py
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -1,19 +1,19 @@
-# Last updated: 3/28/2026, 12:54:29 AM
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter=Counter(tasks)
-        maxHeap=[-cnt for cnt in counter.values()]
-        heapq.heapify(maxHeap)
-
-        time=0
-        q=deque()
-        while maxHeap or q:
-            time+=1
-            if maxHeap:
-                cnt=1+heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt,time+n])
-            if q and q[0][1]==time:
-                heapq.heappush(maxHeap,q.popleft()[0])
-        return time
-        
+# Last updated: 3/28/2026, 9:35:05 PM
+1class Solution:
+2    def leastInterval(self, tasks: List[str], n: int) -> int:
+3        counter=Counter(tasks)
+4        maxHeap=[-cnt for cnt in counter.values()]
+5        heapq.heapify(maxHeap)
+6
+7        time=0
+8        q=deque()
+9        while maxHeap or q:
+10            time+=1
+11            if maxHeap:
+12                cnt=1+heapq.heappop(maxHeap)
+13                if cnt:
+14                    q.append([cnt,time+n])
+15            if q and q[0][1]==time:
+16                heapq.heappush(maxHeap,q.popleft()[0])
+17        return time
+18        
