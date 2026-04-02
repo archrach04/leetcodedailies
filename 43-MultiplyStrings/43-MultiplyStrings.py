22afda5a6448f62e35fa2dfcf89bf72a2c9e677e
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-04-02 23:02:28 +0530

    File updated at 4/2/2026, 11:02:24 PM

--- 43-MultiplyStrings/43-MultiplyStrings.py
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -1,29 +1,26 @@
-# Last updated: 4/1/2026, 11:35:51 PM
+# Last updated: 4/2/2026, 11:02:24 PM
 1class Solution:
 2    def multiply(self, num1: str, num2: str) -> str:
-3        if num1 == "0" or num2 == "0":
+3        if num1=="0" or num2=="0":
 4            return "0"
-5        
-6        n, m = len(num1), len(num2)
-7        res = [0] * (n + m)
-8        
-9        # Multiply digits
-10        for i in range(n - 1, -1, -1):
-11            for j in range(m - 1, -1, -1):
-12                mul = int(num1[i]) * int(num2[j])
-13                
-14                p1 = i + j
-15                p2 = i + j + 1
-16                
-17                total = mul + res[p2]
-18                
-19                res[p2] = total % 10
-20                res[p1] += total // 10
-21        
-22        # Convert to string (skip leading zeros)
-23        result = []
-24        for num in res:
-25            if not (len(result) == 0 and num == 0):
-26                result.append(str(num))
-27        
-28        return "".join(result)
+5        n=len(num1)
+6        m=len(num2)
+7        res=[0]*(n+m)
+8        for i in range(n-1,-1,-1):
+9            for j in range(m-1,-1,-1):
+10                mul=int(num1[i])*int(num2[j])
+11
+12                curr=i+j+1
+13                carr=i+j
+14
+15                tot=res[curr]+mul
+16                res[curr]=tot%10
+17                res[carr]+=tot//10
+18        result=[]
+19        for nums in res:
+20            if not (nums==0 and len(result)==0):
+21                result.append(str(nums))
+22        return ''.join(result)
+23
+24
+25        
