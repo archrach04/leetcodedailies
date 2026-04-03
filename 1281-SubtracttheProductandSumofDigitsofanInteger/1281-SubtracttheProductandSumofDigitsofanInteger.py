803b4fb4a43748e9b3537464bf404cc6999e225a
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-04-04 01:36:31 +0530

    Create 1281-SubtracttheProductandSumofDigitsofanInteger

---
+++ 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.py
@@ -0,0 +1,11 @@
+# Last updated: 4/4/2026, 1:36:26 AM
+1class Solution:
+2    def subtractProductAndSum(self, n: int) -> int:
+3        prod=1
+4        s=0
+5        while n>0:
+6            prod*=n%10
+7            s+=n%10
+8            n=n//10
+9        return prod-s
+10        
