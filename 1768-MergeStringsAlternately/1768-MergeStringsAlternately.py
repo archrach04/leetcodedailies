955aa3a93c7f2c99c97d4d7cdc3b371ae36b9ff4
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-04-29 18:17:48 +0530

    File updated at 4/29/2026, 6:17:24 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,4 +1,4 @@
-# Last updated: 4/29/2026, 6:15:24 PM
+# Last updated: 4/29/2026, 6:17:24 PM
 1class Solution:
 2    def maxVowels(self, s: str, k: int) -> int:
 3        l=0
@@ -11,19 +11,21 @@
 10                m+=1
 11        maxi=m
 12        while r<len(s):
-13            if s[r] in vset:
-14                m+=1
-15            if s[l] in vset:
-16                m-=1
-17            l+=1
-18            r+=1
-19            maxi=max(m,maxi)
-20            if maxi==k:
-21                return k
-22        
-23        return maxi
-24
-25
+13            if s[l] in vset:
+14                m-=1
+15            l+=1
+16        
+17            if s[r] in vset:
+18                m+=1
+19
+20            r+=1
+21            maxi=max(m,maxi)
+22            if maxi==k:
+23                return k
+24        
+25        return maxi
 26
 27
-28        
+28
+29
+30        
