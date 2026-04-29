7755f81773ccac13ac04562cd1796366d75bb476
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-04-29 18:15:48 +0530

    File updated at 4/29/2026, 6:15:24 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,23 +1,29 @@
-# Last updated: 4/28/2026, 1:14:28 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        if head is None or head.next is None:
-9            return head
-10        odd=head
-11        even=even_head=head.next
-12
-13        while even and even.next:
-14            odd.next=even.next
-15            odd=odd.next
-16            even.next=odd.next
-17            even=even.next
-18        odd.next=even_head
-19        return head
-20
-21
+# Last updated: 4/29/2026, 6:15:24 PM
+1class Solution:
+2    def maxVowels(self, s: str, k: int) -> int:
+3        l=0
+4        r=k
+5        vset={'a','e','i','o','u'}
+6        maxi=0
+7        m=0
+8        for i in range(k):
+9            if s[i] in vset:
+10                m+=1
+11        maxi=m
+12        while r<len(s):
+13            if s[r] in vset:
+14                m+=1
+15            if s[l] in vset:
+16                m-=1
+17            l+=1
+18            r+=1
+19            maxi=max(m,maxi)
+20            if maxi==k:
+21                return k
 22        
+23        return maxi
+24
+25
+26
+27
+28        
