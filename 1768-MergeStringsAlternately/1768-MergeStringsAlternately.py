23db483d3d6d1c51a7342b0bf7f03080c7f673dc
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-04-30 22:47:21 +0530

    File updated at 4/30/2026, 10:46:56 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,31 +1,19 @@
-# Last updated: 4/29/2026, 6:17:24 PM
+# Last updated: 4/30/2026, 10:46:56 PM
 1class Solution:
-2    def maxVowels(self, s: str, k: int) -> int:
-3        l=0
-4        r=k
-5        vset={'a','e','i','o','u'}
-6        maxi=0
-7        m=0
-8        for i in range(k):
-9            if s[i] in vset:
-10                m+=1
-11        maxi=m
-12        while r<len(s):
-13            if s[l] in vset:
-14                m-=1
-15            l+=1
-16        
-17            if s[r] in vset:
-18                m+=1
-19
-20            r+=1
-21            maxi=max(m,maxi)
-22            if maxi==k:
-23                return k
-24        
-25        return maxi
-26
-27
-28
-29
-30        
+2    def uniqueOccurrences(self, arr: List[int]) -> bool:
+3        hashm={}
+4        s=set()
+5        for i in arr:
+6            if i in hashm:
+7                hashm[i]+=1
+8            else:
+9                hashm[i]=1
+10        for i in hashm:
+11            if hashm[i] in s:
+12                return False
+13            else:
+14                s.add(hashm[i])
+15        
+16        return True
+17
+18        
