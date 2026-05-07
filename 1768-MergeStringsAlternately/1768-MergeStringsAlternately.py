d29b3cc70172f9440e8651adda0f7574346ad79c
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-05-07 23:02:57 +0530

    File updated at 5/7/2026, 11:02:31 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,16 +1,22 @@
-# Last updated: 5/1/2026, 10:57:42 PM
+# Last updated: 5/7/2026, 11:02:31 PM
 1class Solution:
-2    def removeStars(self, s: str) -> str:
-3        st=[]
-4        word=""
-5        for i in range(len(s)):
-6            if s[i]=="*":
-7                st.pop()
-8            else:
-9                st.append(s[i])
-10        for i in st:
-11            word+=i
-12        
-13        return word
-14
-15        
+2    def longestSubarray(self, nums: List[int]) -> int:
+3
+4        start=0
+5        max_len=0
+6        zero=-1
+7
+8        for r in range(len(nums)):
+9            if nums[r]==0:
+10                maxnow=0
+11                start=zero+1
+12                zero=r
+13
+14            maxnow=r-start
+15            max_len=max(max_len,maxnow)
+16        return max_len
+17           
+18        
+19
+20
+21        
