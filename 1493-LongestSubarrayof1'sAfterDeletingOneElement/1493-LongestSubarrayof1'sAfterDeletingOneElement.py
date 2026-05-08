ebfe4a110b7c8e54a7c4f87cd4b24becf234183f
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-05-08 22:37:45 +0530

    Create 1493-LongestSubarrayof1'sAfterDeletingOneElement

---
+++ 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.py
@@ -0,0 +1,22 @@
+# Last updated: 5/8/2026, 10:37:18 PM
+1class Solution:
+2    def longestSubarray(self, nums: List[int]) -> int:
+3
+4        start=0
+5        max_len=0
+6        zero=-1
+7
+8        for r in range(len(nums)):
+9            if nums[r]==0:
+10                maxnow=0
+11                start=zero+1
+12                zero=r
+13
+14            maxnow=r-start
+15            max_len=max(max_len,maxnow)
+16        return max_len
+17           
+18        
+19
+20
+21        
