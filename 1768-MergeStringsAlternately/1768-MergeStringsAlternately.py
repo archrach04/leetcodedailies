65ac0f45a187fb0ac3af3e817a7443214a3b465d
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-05-09 16:14:43 +0530

    File updated at 5/9/2026, 4:14:17 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,22 +1,16 @@
-# Last updated: 5/7/2026, 11:02:31 PM
+# Last updated: 5/9/2026, 4:14:17 PM
 1class Solution:
-2    def longestSubarray(self, nums: List[int]) -> int:
-3
-4        start=0
-5        max_len=0
-6        zero=-1
-7
-8        for r in range(len(nums)):
-9            if nums[r]==0:
-10                maxnow=0
-11                start=zero+1
-12                zero=r
-13
-14            maxnow=r-start
-15            max_len=max(max_len,maxnow)
-16        return max_len
-17           
-18        
-19
-20
-21        
+2    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+3        visited=set()
+4        stack=[0]
+5        while stack:
+6            room=stack.pop()
+7            visited.add(room)
+8            for key in rooms[room]:
+9                if key not in visited:
+10                    stack.append(key)
+11        if len(visited)==len(rooms):
+12             return True
+13        else: 
+14            return False
+15                    
