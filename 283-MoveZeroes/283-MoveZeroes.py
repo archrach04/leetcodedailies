b58c174e56982f24864fe3ce0c397f866b3168d6
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-08-02 18:19:09 +0530

    File updated at 8/2/2026, 6:19:10 PM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,11 +1,17 @@
-# Last updated: 3/28/2026, 12:54:47 AM
-class Solution:
-    def moveZeroes(self, nums: list[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        l = 0
-        for r in range(len(nums)):
-            if nums[r]:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
+# Last updated: 8/2/2026, 6:19:10 PM
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5
+6        """
+7
+8        l=0
+9        for r in range(len(nums)):
+10            if nums[r]!=0:
+11                nums[l],nums[r]=nums[r],nums[l]
+12                l+=1
+13        return nums
+14
+15
+16        
