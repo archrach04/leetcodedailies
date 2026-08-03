cba80f03949415857cbee7c08f9e2aa98d117f97
archrach04 <146434907+archrach04@users.noreply.github.com>
2026-08-03 21:31:36 +0530

    File updated at 8/3/2026, 9:31:36 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,27 +1,31 @@
-# Last updated: 3/28/2026, 12:55:52 AM
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        word = ""
-        maxlen = 0
-
-        for i in range(len(s)):
-            # odd
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > maxlen:
-                    word = s[l:r+1]
-                    maxlen = r - l + 1
-
-                l -= 1
-                r += 1
-
-            # even
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > maxlen:
-                    word = s[l:r+1]
-                    maxlen = r - l +1
-                l -= 1
-                r += 1
-
-        return word
+# Last updated: 8/3/2026, 9:31:36 PM
+1class Solution:
+2    def longestPalindrome(self, s: str) -> str:
+3        n=len(s)
+4        lps=""
+5
+6        for i in range(n):
+7
+8            low=i
+9            high=i
+10
+11            while low>=0 and high<len(s) and s[low]==s[high]:
+12                low-=1
+13                high+=1
+14            
+15            if len(s[low+1:high])>len(lps):
+16                lps=s[low+1:high]
+17            
+18            low=i
+19            high=i+1
+20
+21            while low>=0 and high<len(s) and s[low]==s[high]:
+22                low-=1
+23                high+=1
+24
+25            if len(s[low+1:high])>len(lps):
+26                lps=s[low+1:high]
+27        
+28        
+29        return lps
+30        
